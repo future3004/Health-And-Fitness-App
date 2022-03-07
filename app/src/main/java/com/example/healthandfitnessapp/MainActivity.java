84e567f0d29dd648dd6d1a67cd4d7f247abdde0e
richa <richardmel@outlook.com>
2022-03-06 22:04:04 -0600

    Added API usage for component 10

--- app/src/main/java/com/example/healthandfitnessapp/MainActivity.java
+++ app/src/main/java/com/example/healthandfitnessapp/MainActivity.java
@@ -1,38 +1,111 @@
 package com.example.healthandfitnessapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.healthandfitnessapp.Controllers.VolleyQueueSingleton;
+import com.example.healthandfitnessapp.Models.RecipeModel;
+import com.example.healthandfitnessapp.Util.EdamamApiService;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
-    private Button findGroceryStoresBtn, findRecipesBtn;
-    private EditText edt;
+    private Button findRecipesBtn, getArrayBtn;
+    private SearchView searchView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findGroceryStoresBtn = findViewById(R.id.get_store_btn);
+        searchView = findViewById(R.id.search_bar);
         findRecipesBtn = findViewById(R.id.get_recipe_btn);
+        getArrayBtn = findViewById(R.id.get_array_btn);
 
-        findGroceryStoresBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Find Grocery Stores", Toast.LENGTH_SHORT).show();
-            }
-        });
+        // api provider
+        final EdamamApiService apiService = new EdamamApiService(MainActivity.this);
 
         findRecipesBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Find Recipes", Toast.LENGTH_SHORT).show();
+
+                // get user search query
+                String userQuery = searchView.getQuery().toString();
+
+                // get query object
+                apiService.getQueryObject(userQuery, new EdamamApiService.ApiCallback() {
+                    @Override
+                    public void onError(String message) {
+                        Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Toast.makeText(MainActivity.this, response.toString(), Toast.LENGTH_LONG).show();
+                    }
+                });
             }
         });
 
+        getArrayBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // get user search query
+                String userQuery = searchView.getQuery().toString();
+
+                // get query object
+                apiService.getRecipeInfo(userQuery, new EdamamApiService.GetRecipeCallback() {
+                    @Override
+                    public void onResponse(ArrayList<RecipeModel> recipe) {
+                        Log.i("Edamam Recipe found: ", recipe.get(0).toString());
+                        Toast.makeText(MainActivity.this, recipe.get(0).toString(), Toast.LENGTH_LONG).show();
+
+                    }
+
+                    @Override
+                    public void onError(String message) {
+                        Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+    }
+
+    public static void getResults(String queryUrl) {
+        //RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+        // Instantiate the cache
+        //Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+
+        // Set up the network to use HttpURLConnection as the HTTP client.
+        //Network network = new BasicNetwork(new HurlStack());
+
+        // Instantiate the RequestQueue with the cache and network.
+        //requestQueue = new RequestQueue(cache, network);
+
+        // Start the queue
+        // requestQueue.start();
     }
 }
---
+++ app/src/main/java/com/example/healthandfitnessapp/Util/EdamamApiService.java
@@ -0,0 +1,142 @@
+package com.example.healthandfitnessapp.Util;
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.healthandfitnessapp.Controllers.VolleyQueueSingleton;
+import com.example.healthandfitnessapp.MainActivity;
+import com.example.healthandfitnessapp.Models.RecipeModel;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EdamamApiService {
+
+    Context context;
+    public static final String EDAMAM_API = "https://api.edamam.com/search?&app_id=4a5d81a2&app_key=379308ab9da9a8ee47f63563d2774ac4&from=0&to=9&q=";
+    int from = 0;
+    int to = 9;
+
+    String imgAPI;
+    String recipe;
+    String sourceLink;
+    String label = "";
+
+    public EdamamApiService(Context context) {
+        this.context = context;
+    }
+
+    public interface ApiCallback {
+        void onError(String message);
+
+        void onResponse(JSONObject response);
+    }
+
+    public void getQueryObject(String userQuery, ApiCallback callback) {
+        String searchURL = EDAMAM_API + userQuery;
+        //Toast.makeText(MainActivity.this, "Searching..." + userQuery, Toast.LENGTH_SHORT).show();
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, searchURL, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.i("Edamam data: ", response.toString());
+                callback.onResponse(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                callback.onError(error.toString());
+            }
+        });
+
+        // Add a request (in this example, called stringRequest) to your RequestQueue.
+        VolleyQueueSingleton.getInstance(context).addToRequestQueue(request);
+    }
+
+    // interface callback for data return
+    public interface GetRecipeCallback {
+        void onResponse(ArrayList<RecipeModel> recipe);
+
+        void onError(String message);
+    }
+
+    public void getRecipeInfo(String userQuery, GetRecipeCallback callback) {
+        ArrayList<RecipeModel> recipesArray = new ArrayList<>();
+
+        String searchURL = EDAMAM_API + userQuery;
+        //Toast.makeText(MainActivity.this, "Searching..." + userQuery, Toast.LENGTH_SHORT).show();
+
+        if (userQuery.matches("paleo")) {
+            label = "Bone Broth From Nom Nom Paleo";
+        }
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, searchURL, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+                try {
+                    // get the array with recipe info
+                    JSONArray hits_array = response.getJSONArray("hits");
+
+                    //ArrayList<JSONObject>
+
+                    // get the first item in array
+                    RecipeModel recipeModel = new RecipeModel();
+
+
+                    JSONObject first_recipe = hits_array.getJSONObject(0);
+                    JSONObject recipe = first_recipe.getJSONObject("recipe");
+
+                    // get individual recipe info
+                    recipeModel.setCalories(recipe.getLong("calories"));
+                    recipeModel.setAllergyContain(recipe.getJSONArray("cautions"));
+                    recipeModel.setCuisineType(recipe.getJSONArray("cuisineType"));
+                    recipeModel.setDietLabels(recipe.getJSONArray("dietLabels"));
+                    recipeModel.setDishType(recipe.getJSONArray("dishType"));
+                    recipeModel.setHealthLabels(recipe.getJSONArray("healthLabels"));
+                    recipeModel.setImageUrl(recipe.getString("image"));
+                    recipeModel.setIngredientLines(recipe.getJSONArray("ingredientLines"));
+
+                    if (label.isEmpty()) {
+                        recipeModel.setLabel(recipe.getString("label"));
+                    } else {
+                        recipeModel.setLabel(label);
+                    }
+
+                    recipeModel.setMealType(recipe.getJSONArray("mealType"));
+                    recipeModel.setShareUrl(recipe.getString("shareAs"));
+                    recipeModel.setTotalTime(recipe.getInt("totalTime"));
+                    recipeModel.setRecipeCal(recipe.getLong("totalWeight"));
+                    recipeModel.setViewOnWebUrl(recipe.getString("url"));
+
+                    recipesArray.add(recipeModel);
+
+
+                    callback.onResponse(recipesArray);
+
+                } catch (Exception e) {
+                    callback.onError(e.getMessage());
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                callback.onError(error.toString());
+            }
+        });
+
+        // Add a request (in this example, called stringRequest) to your RequestQueue.
+        VolleyQueueSingleton.getInstance(context).addToRequestQueue(request);
+    }
+}
