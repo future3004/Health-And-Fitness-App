7f4b59428ca36884ee9ed47f4071a6d10d45458a
jaredbenson1 <100323666+jaredbenson1@users.noreply.github.com>
2022-03-11 20:57:28 -0600

    Add files via upload

---
+++ logindemo/FirstTimeSignUp.java
@@ -0,0 +1,14 @@
+package com.example.logindemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FirstTimeSignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_time_sign_up);
+    }
+}
---
+++ logindemo/GettingStarted.java
@@ -0,0 +1,17 @@
+package com.example.logindemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class GettingStarted extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_getting_started);
+
+    }
+}
