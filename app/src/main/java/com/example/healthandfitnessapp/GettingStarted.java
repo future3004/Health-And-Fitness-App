3919d3819416a730cdd579329d4519afe2f52298
jaredbenson1 <100323666+jaredbenson1@users.noreply.github.com>
2022-05-10 09:56:08 -0500

    Update GettingStarted.java

--- app/src/main/java/com/example/healthandfitnessapp/GettingStarted.java
+++ app/src/main/java/com/example/healthandfitnessapp/GettingStarted.java
@@ -1,13 +1,18 @@
 package com.example.healthandfitnessapp;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -23,8 +28,8 @@ import java.util.Calendar;
 
 public class GettingStarted extends AppCompatActivity {
 
-    private DatePickerDialog datePickerDialog;
-    private Button dateButton;
+    private TextView mDisplayDate;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
 
     TextView usernameEntry;
 
@@ -34,15 +39,44 @@ public class GettingStarted extends AppCompatActivity {
     EditText currentWeightEntryChoice;
     EditText goalWeightEntryChoice;
     MaterialButton submitButtonGettingStartedPage;
-    String username = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_getting_started);
 
+        mDisplayDate = (TextView) findViewById(R.id.tvDate);
+
+        mDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                //month = month + 1;
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(GettingStarted.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener,
+                        year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                Log.d(TAG, "onDateSet: mm/dd/yyyy: " + month + "/" + day + "/" + year);
+                String date = month + "/" + day + "/" + year;
+                mDisplayDate.setText(date);
+            }
+        };
+
         Intent intent = getIntent();
-        username = intent.getStringExtra("username");
+        String username = intent.getStringExtra("username");
 
         // set toolbar
         ActionBar actionBar = getSupportActionBar();
@@ -81,6 +115,73 @@ public class GettingStarted extends AppCompatActivity {
         });
 
     }
+
+//    private String getTodaysDate(){
+//        Calendar cal = Calendar.getInstance();
+//        int year = cal.get(Calendar.YEAR);
+//        int month = cal.get(Calendar.MONTH);
+//        month = month + 1;
+//        int day = cal.get(Calendar.DAY_OF_MONTH);
+//        return makeDateString(day, month, year);
+//    }
+//
+//    private void initDatePicker() {
+//        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+//            @Override
+//            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+//                month = month + 1;
+//                String date = makeDateString(day, month, year);
+//                dateButton.setText(date);
+//            }
+//        };
+//
+//        Calendar cal = Calendar.getInstance();
+//        int year = cal.get(Calendar.YEAR);
+//        int month = cal.get(Calendar.MONTH);
+//        int day = cal.get(Calendar.DAY_OF_MONTH);
+//
+//        int style = AlertDialog.THEME_HOLO_LIGHT;
+//
+//        datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
+//        datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
+//    }
+//
+//    private String makeDateString(int day, int month, int year){
+//        return getMonthFormat(month) + " " + day + " " + year;
+//    }
+//
+//    private String getMonthFormat(int month){
+//        if(month == 1)
+//            return "JAN";
+//        if(month == 2)
+//            return "FEB";
+//        if(month == 3)
+//            return "MAR";
+//        if(month == 4)
+//            return "APR";
+//        if(month == 5)
+//            return "MAY";
+//        if(month == 6)
+//            return "JUN";
+//        if(month == 7)
+//            return "JUL";
+//        if(month == 8)
+//            return "AUG";
+//        if(month == 9)
+//            return "SEP";
+//        if(month == 10)
+//            return "OCT";
+//        if(month == 11)
+//            return "NOV";
+//        if(month == 12)
+//            return "DEC";
+//        //default
+//        return "JAN";
+//    }
+//
+//    public void openDatePicker(View view) {
+//        datePickerDialog.show();
+//    }
     private void createUserStats() {
 /*        String age = ageEntryChoice.getText().toString();
         int ageInt = Integer.parseInt(age);
@@ -91,6 +192,7 @@ public class GettingStarted extends AppCompatActivity {
         String goalWeight = goalWeightEntryChoice.getText().toString();
         int goalWeightInt = Integer.parseInt(goalWeight);*/
 
+        openHomePage();
 
 /*        if(TextUtils.isEmpty(age)){
             ageEntryChoice.setError("age cannot be empty");
@@ -108,74 +210,13 @@ public class GettingStarted extends AppCompatActivity {
             openHomePage();
         }*/
 
-        openHomePage();
     }
 
-    private String getTodaysDate(){
-        Calendar cal = Calendar.getInstance();
-        int year = cal.get(Calendar.YEAR);
-        int month = cal.get(Calendar.MONTH);
-        month = month + 1;
-        int day = cal.get(Calendar.DAY_OF_MONTH);
-        return makeDateString(day, month, year);
-    }
 
-    private void initGoalDatePicker() {
-        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                month = month + 1;
-                String date = makeDateString(day, month, year);
-                dateButton.setText(date);
-            }
-        };
-
-        Calendar cal = Calendar.getInstance();
-        int year = cal.get(Calendar.YEAR);
-        int month = cal.get(Calendar.MONTH);
-        int day = cal.get(Calendar.DAY_OF_MONTH);
-
-        int style = AlertDialog.THEME_HOLO_LIGHT;
-
-        datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
-        datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
-    }
-
-    private String makeDateString(int day, int month, int year){
-        return getMonthFormat(month) + " " + day + " " + year;
-    }
-
-    private String getMonthFormat(int month){
-        if(month == 1)
-            return "JAN";
-        if(month == 2)
-            return "FEB";
-        if(month == 3)
-            return "MAR";
-        if(month == 4)
-            return "APR";
-        if(month == 5)
-            return "MAY";
-        if(month == 6)
-            return "JUN";
-        if(month == 7)
-            return "JUL";
-        if(month == 8)
-            return "AUG";
-        if(month == 9)
-            return "SEP";
-        if(month == 10)
-            return "OCT";
-        if(month == 11)
-            return "NOV";
-        if(month == 12)
-            return "DEC";
-        //default
-        return "JAN";
-    }
-
-    private void openHomePage(){
+    public void openHomePage(){
         Intent intent = new Intent(GettingStarted.this, HomePage.class);
         startActivity(intent);
     }
+
+
 }
